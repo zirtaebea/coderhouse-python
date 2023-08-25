8d000944578d0abc1b813680c4f82ce6e3f7a355
zirtaebea <zirtaeb.dev@gmail.com>
2023-08-24 22:21:00 -0300

    Desafio Obrigatório 01

--- 00_programas/00_03_desafios_obrigatorios/do_01_notify.py
+++ 00_programas/00_03_desafios_obrigatorios/do_01_notify.py
@@ -3,6 +3,10 @@
 from plyer import notification 
 from datetime import datetime
 
+meu_nivel = int(input("Insira o número correspondente ao nível do alerta: \n 1 - BAIXO \n 2 - MÉDIO \n 3 - ALTO\n"))
+minha_base = input("Insira a base que ocorreu o conflito: ")
+minha_etapa = input("Insira a etapa na qual ocorreu o conflito: ")
+
 #definindo função de alerta
 def alerta(nivel, base, etapa):
     
@@ -23,10 +27,10 @@ def alerta(nivel, base, etapa):
     notification.notify(
     title=f"ATENÇÃO: {tipo_alerta}",  # título do alerta
     message="Falha no carregamento da base {} na etapa {}.\n{}".format(base, etapa, str(datetime.now())),  # mensagem do alerta
-    app_name="Aplicativo X",
+    app_name="Base de dados",
     timeout=10
     )
 
 #testando função
-alerta(3, 'Clientes', 'Extração')
+alerta(meu_nivel, minha_base, minha_etapa)
 
